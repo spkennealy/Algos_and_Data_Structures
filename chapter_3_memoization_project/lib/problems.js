f3f66837834c40c2fba811812abc4b0d47751a82
Sean Kennealy <spkennealy@gmail.com>
2019-07-09 15:08:57 -0700

    Complete problem 1 in memoization project

--- chapter_3_memoization_project/lib/problems.js
+++ chapter_3_memoization_project/lib/problems.js
@@ -16,7 +16,12 @@
 // lucasNumberMemo(41)  // => 370248451
 // lucasNumberMemo(42)  // => 599074578
 function lucasNumberMemo(n, memo = {}) {
-
+    if (n in memo) return memo[n];
+    if (n === 0) return 2;
+    if (n === 1) return 1;
+    let res = lucasNumberMemo(n - 2, memo) + lucasNumberMemo(n - 1, memo);
+    memo[n] = res;
+    return memo[n];
 }
 
 
