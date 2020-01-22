841840d0bc139a08850723131fde3d47f1c4b3e7
Sean Kennealy <spkennealy@gmail.com>
2020-01-22 08:08:21 -0800

    Completed selection sort project

--- chapter_6_selection_sort_project/selection_sort_project/lib/selection_sort.js
+++ chapter_6_selection_sort_project/selection_sort_project/lib/selection_sort.js
@@ -6,7 +6,19 @@ function swap(arr, index1, index2) {
 }
 
 function selectionSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let min = i;
 
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[i]) {
+                min = j;
+            }
+        }
+
+        swap(arr, i, min);
+    }
+
+    return arr;
 }
 
 module.exports = {
