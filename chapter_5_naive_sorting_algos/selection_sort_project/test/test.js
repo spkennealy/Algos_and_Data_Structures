cb7a45e846d0107c22fb6b696c00c927eab35c98
Sean Kennealy <spkennealy@gmail.com>
2020-01-22 08:16:10 -0800

    Move folder for naive sorting algos

--- chapter_5_naive_sorting_algos/selection_sort_project/test/test.js
+++ chapter_5_naive_sorting_algos/selection_sort_project/test/test.js
@@ -1,27 +1,26 @@
 const { expect } = require('chai');
-const { bubbleSort, swap } = require('../lib/bubble_sort.js');
+const { selectionSort, swap } = require('../lib/selection_sort.js.js.js.js');
 
 describe('swap()', () => {
     // it should accept an array and two indices as args
 
     it('should swap the elements at the given indices, mutating the original array', () => {
-        let array = ['a', 'b', 'c'];
-        swap(array, 0, 2);
-        expect(array).to.eql(['c', 'b', 'a']);
-    });
+        let array1 = ['a', 'b', 'c'];
+        swap(array1, 0, 2);
+        expect(array1).to.eql(['c', 'b', 'a']);
 
-    it('should return the original array', () => {
-        let array = ['a', 'b', 'c'];
-        expect(swap(array, 1, 0)).to.equal(array);
+        let array2 = ['a', 'b', 'c'];
+        swap(array2, 1, 0);
+        expect(array2).to.eql(['b', 'a', 'c']);
     });
 });
 
-describe('bubbleSort()', () => {
+describe('selectionSort()', () => {
     // it should accept an array of numbers as an arg
 
     it('should sort the elements of the array in increasing order, in-place', () => {
         let array = [2, -1, 4, 3, 7, 3];
-        bubbleSort(array);
+        selectionSort(array);
         expect(array).to.eql([-1, 2, 3, 3, 4, 7]);
     });
 });
