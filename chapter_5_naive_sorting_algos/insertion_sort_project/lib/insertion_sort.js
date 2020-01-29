b1325333c9e77f356da06a9eb5069f6e2955c26e
Sean Kennealy <spkennealy@gmail.com>
2020-01-29 09:21:04 -0800

    Completed insertion sort project

--- chapter_5_naive_sorting_algos/insertion_sort_project/lib/insertion_sort.js
+++ chapter_5_naive_sorting_algos/insertion_sort_project/lib/insertion_sort.js
@@ -1,5 +1,16 @@
 function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++)
+    {
+        let currEl = arr[i];
+        for (var j = i - 1; j >= 0 && currEl < arr[j]; j--)
+        {
+            arr[j + 1] = arr[j];
+        }
 
+        arr[j + 1] = currEl;
+    }
+
+    return arr;
 }
 
 module.exports = {
