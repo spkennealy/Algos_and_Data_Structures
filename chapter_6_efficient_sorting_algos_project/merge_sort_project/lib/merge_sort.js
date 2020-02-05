b55f1aff11234b52e977b834c67b3147d850d431
Sean Kennealy <spkennealy@gmail.com>
2020-02-04 17:32:31 -0800

    Merge Sort Project: completed merge helper function

--- chapter_6_efficient_sorting_algos_project/merge_sort_project/lib/merge_sort.js
+++ chapter_6_efficient_sorting_algos_project/merge_sort_project/lib/merge_sort.js
@@ -1,5 +1,19 @@
 function merge(array1, array2) {
+    let merged = [];
 
+    while (array1.length || array2.length)
+    {
+        if (array1[0] > array2[0])
+        {
+            merged.push(array2.shift);
+        }
+        else
+        {
+            merged.push(array1.shift);
+        }
+    }
+
+    return merged.contact(array1.concat(array2));
 }
 
 function mergeSort(array) {
