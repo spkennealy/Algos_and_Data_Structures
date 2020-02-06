14485b3eb0cf90e86467d58912cd027f6da6e38a
Sean Kennealy <spkennealy@gmail.com>
2020-02-05 17:04:35 -0800

    First attempt at merge sort

--- chapter_6_efficient_sorting_algos_project/merge_sort_project/lib/merge_sort.js
+++ chapter_6_efficient_sorting_algos_project/merge_sort_project/lib/merge_sort.js
@@ -1,14 +1,10 @@
 function merge(array1, array2) {
     let merged = [];
 
-    while (array1.length || array2.length)
-    {
-        if (array1[0] > array2[0])
-        {
+    while (array1.length || array2.length) {
+        if (array1[0] > array2[0]) {
             merged.push(array2.shift);
-        }
-        else
-        {
+        } else {
             merged.push(array1.shift);
         }
     }
@@ -17,7 +13,15 @@ function merge(array1, array2) {
 }
 
 function mergeSort(array) {
+    if (array.length == 1) {
+        return array;
+    }
 
+    let mid = array.length / 2;
+    let left = mergeSort(array.slice(0, mid));
+    let right = mergeSort(array.slice(mid, array.length));
+
+    return merge(left, right);
 }
 
 module.exports = {
