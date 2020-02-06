1593979b316f6d42c9ec7a48fbeb8fe79b31c797
Sean Kennealy <spkennealy@gmail.com>
2020-02-05 17:11:39 -0800

    Completed merge sort

--- chapter_6_efficient_sorting_algos_project/merge_sort_project/lib/merge_sort.js
+++ chapter_6_efficient_sorting_algos_project/merge_sort_project/lib/merge_sort.js
@@ -2,24 +2,30 @@ function merge(array1, array2) {
     let merged = [];
 
     while (array1.length || array2.length) {
-        if (array1[0] > array2[0]) {
-            merged.push(array2.shift);
+        let ele1 = array1.length ? array1[0] : Infinity;
+        let ele2 = array2.length ? array2[0] : Infinity;
+
+        let next;
+        if (ele1 < ele2) {
+            next = array1.shift();
         } else {
-            merged.push(array1.shift);
+            next = array2.shift();
         }
+
+        merged.push(next);
     }
 
-    return merged.contact(array1.concat(array2));
+    return merged;
 }
 
 function mergeSort(array) {
-    if (array.length == 1) {
+    if (array.length <= 1) {
         return array;
     }
 
-    let mid = array.length / 2;
+    let mid = Math.floor(array.length / 2);
     let left = mergeSort(array.slice(0, mid));
-    let right = mergeSort(array.slice(mid, array.length));
+    let right = mergeSort(array.slice(mid));
 
     return merge(left, right);
 }
