9b4a86fadd736fdc264879a04135c83d12cf2bec
Sean Kennealy <spkennealy@gmail.com>
2020-02-26 08:26:52 -0800

    Completed quicksort

--- chapter_6_efficient_sorting_algos_project/quick_sort_project/lib/quick_sort.js
+++ chapter_6_efficient_sorting_algos_project/quick_sort_project/lib/quick_sort.js
@@ -3,11 +3,14 @@ function quickSort(array) {
         return array;
     }
 
-    let pivot = array.slice();
+    let pivot = array.shift();
     let left = array.filter(a => a < pivot);
     let right = array.filter(b => b >= pivot);
 
-    
+    let leftSorted = quickSort(left);
+    let rightSorted = quickSort(right);
+
+    return [...leftSorted, pivot, ...rightSorted];
 }
 
 
