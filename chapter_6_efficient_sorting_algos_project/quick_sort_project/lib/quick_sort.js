3bcea7eb7ba2575f7d5571165b07a384d30c1f68
Sean Kennealy <spkennealy@gmail.com>
2020-02-26 08:22:42 -0800

    Quicksort, first half of algo

--- chapter_6_efficient_sorting_algos_project/quick_sort_project/lib/quick_sort.js
+++ chapter_6_efficient_sorting_algos_project/quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,13 @@
 function quickSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
 
+    let pivot = array.slice();
+    let left = array.filter(a => a < pivot);
+    let right = array.filter(b => b >= pivot);
+
+    
 }
 
 
