28fef81763344b2246f04a527e5f8fba39105fcb
Sean Kennealy <spkennealy@gmail.com>
2020-03-31 08:28:53 -0700

    Radix Sort Project: getMaxDigits

--- chapter_6_efficient_sorting_algos_project/radix_sort_project/lib/radix_sort.js
+++ chapter_6_efficient_sorting_algos_project/radix_sort_project/lib/radix_sort.js
@@ -10,6 +10,14 @@ const getDigitFrom = (num, place) => Math.floor(Math.abs(num)) / Math.pow(10, pl
 
 const getIntLength = (num) => (num == 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
 
+const getMaxDigits = (nums) => {
+    let maxDigits = 0;
+    for (let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, getIntLength(nums[i]));
+    }
+    return maxDigits;
+}
+
 module.exports = {
     radixSort
 };
