8a1df4c13b0b105ec3d24dcd93fb30726fbce7a0
Sean Kennealy <spkennealy@gmail.com>
2020-03-31 08:25:49 -0700

    Radix Sort Project: getIntLength

--- chapter_6_efficient_sorting_algos_project/radix_sort_project/lib/radix_sort.js
+++ chapter_6_efficient_sorting_algos_project/radix_sort_project/lib/radix_sort.js
@@ -8,6 +8,7 @@ function radixSort(arr) {
 
 const getDigitFrom = (num, place) => Math.floor(Math.abs(num)) / Math.pow(10, place) % 10;
 
+const getIntLength = (num) => (num == 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
 
 module.exports = {
     radixSort
