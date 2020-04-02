4acae5c4503e4bc0f03578758a64318fbda383b1
Sean Kennealy <spkennealy@gmail.com>
2020-04-02 08:06:28 -0700

    Complete Radix Sort Project

--- chapter_6_efficient_sorting_algos_project/radix_sort_project/lib/radix_sort.js
+++ chapter_6_efficient_sorting_algos_project/radix_sort_project/lib/radix_sort.js
@@ -1,3 +1,15 @@
+const getDigitFrom = (num, place) => Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+
+const getIntLength = (num) => (num === 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
+
+const getMaxDigits = (nums) => {
+    let maxDigits = 0;
+    for (let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, getIntLength(nums[i]));
+    }
+    return maxDigits;
+}
+
 function radixSort(arr) {
     if (!Array.isArray(arr)) {
         return null;
@@ -19,18 +31,8 @@ function radixSort(arr) {
     return arr;
 }
 
-const getDigitFrom = (num, place) => Math.floor(Math.abs(num)) / Math.pow(10, place) % 10;
-
-const getIntLength = (num) => (num == 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
-
-const getMaxDigits = (nums) => {
-    let maxDigits = 0;
-    for (let i = 0; i < nums.length; i++) {
-        maxDigits = Math.max(maxDigits, getIntLength(nums[i]));
-    }
-    return maxDigits;
-}
-
 module.exports = {
     radixSort
 };
+
+radixSort([4, 9, 0, 23, 15, 100, 66, 41, 5, 10]);
