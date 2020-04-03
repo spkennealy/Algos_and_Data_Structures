bd36781b01b95254adb746df636e2e6be18cdbf8
Sean Kennealy <spkennealy@gmail.com>
2020-04-03 09:36:47 -0700

    Completed Counting Sort Project

--- chapter_6_efficient_sorting_algos_project/counting_sort_project/lib/counting_sort.js
+++ chapter_6_efficient_sorting_algos_project/counting_sort_project/lib/counting_sort.js
@@ -1,18 +1,16 @@
 function countingSort(arr, max) {
     if (arr.length <= 1) return arr;
     
-    let countArray = Array.from({ length: max }, 0);
+    let countArray = new Array(max + 1).fill(0);
     for (let i = 0; i < arr.length; i++) {
-        countArray[arr[i]] = arr[i];
+        countArray[arr[i]]++;
     }
 
     let result = [];
     for (let j = 0; j < countArray.length; j++) {
-        if (countArray[j] != 0) {
-            while(countArray[j] > 0) {
-                result.push(j);
-                countArray[j]--;
-            }
+        while(countArray[j] > 0) {
+            result.push(j);
+            countArray[j]--;
         }
     }
 
