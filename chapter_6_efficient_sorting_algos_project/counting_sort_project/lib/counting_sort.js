06fe820cb0b303a3c893798e765379ea55e142c7
Sean Kennealy <spkennealy@gmail.com>
2020-04-03 09:14:43 -0700

    First attempt at counting sort

--- chapter_6_efficient_sorting_algos_project/counting_sort_project/lib/counting_sort.js
+++ chapter_6_efficient_sorting_algos_project/counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,22 @@
 function countingSort(arr, max) {
+    if (arr.length <= 1) return arr;
+    
+    let countArray = Array.from({ length: max }, 0);
+    for (let i = 0; i < arr.length; i++) {
+        countArray[arr[i]] = arr[i];
+    }
 
+    let result = [];
+    for (let j = 0; j < countArray.length; j++) {
+        if (countArray[j] != 0) {
+            while(countArray[j] > 0) {
+                result.push(j);
+                countArray[j]--;
+            }
+        }
+    }
+
+    return result;
 }
 
 
