0a82bb51d5f8bcb0d7011c22c1b1a64c3bf52fc2
Sean Kennealy <spkennealy@gmail.com>
2020-04-07 08:39:02 -0700

    BinarySearch completed

--- chapter_7_binary_search_project/lib/binary_search.js
+++ chapter_7_binary_search_project/lib/binary_search.js
@@ -1,9 +1,37 @@
 function binarySearch(array, target) {
+    if (array.length === 0) {
+        return false;
+    }
 
+    let mid = Math.floor(array.length / 2);
+    let left = array.slice(0, mid);
+    let right = array.slice(mid + 1);
+
+    if (target < array[mid]) {
+        return binarySearch(left, target);
+    } else if (target > array[mid]) {
+        return binarySearch(right, target);
+    } else {
+        return true;
+    }
 }
 
 function binarySearchIndex(array, target) {
+    if (array.length === 0) {
+        return -1;
+    }
 
+    let mid = Math.floor(array.length / 2);
+    let left = array.slice(0, mid);
+    let right = array.slice(mid + 1);
+
+    if (target < array[mid]) {
+        return binarySearch(left, target);
+    } else if (target > array[mid]) {
+        return binarySearch(right, target);
+    } else {
+        return mid;
+    }
 }
 
 
