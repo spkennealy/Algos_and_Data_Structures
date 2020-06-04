4c48655ce9d4c1f5f19bdaa94646bba999321451
Sean Kennealy <spkennealy@gmail.com>
2020-06-04 08:31:09 -0700

    Completed StackQueue interview problems

--- chapter_9_stacks_and_queues_project/stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
+++ chapter_9_stacks_and_queues_project/stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
@@ -26,9 +26,68 @@
 // Let's code!
 // -----------
 
+// class Node {
+//     constructor(val) {
+//         this.value = val;
+//         this.next = null;
+//     }
+// }
+
+// class LinkedList {
+//     constructor() {
+//         this.head = null;
+//         this.tail = null;
+//         this.length = 0;
+//     }
+
+//     addToTail(val) {
+//         const newNode = new Node(val);
+
+//         if (!this.head) {
+//             this.head = newNode;
+//         } else {
+//             this.tail.next = newNode;
+//         }
+
+//         this.tail = newNode;
+//         this.length++;
+//         return this;
+//     }
+
+//     get(index) {
+//         if (index < 0 || index >= this.length) return null;
+//         let counter = 0;
+//         let current = this.head;
+//         while (counter !== index) {
+//             current = current.next;
+//             counter++;
+//         }
+//         return current;
+//     }
+// }
+
 function iterateAcrossLinkedListBackwards(linkedList) {
     // TODO: Implement the iterateAcrossLinkedListBackwards function here
+    if (linkedList.length === 0) return "";
 
+    let arr = [];
+    let currNode = linkedList.head;
+    while (currNode) {
+        let val = currNode.value;
+        if (val === null) val = "null";
+        if (val === undefined) val = "undefined";
+        arr.push(val);
+        currNode = currNode.next;
+    }
+
+    return arr.reverse().join(" -> ");
 }
 
 exports.iterateAcrossLinkedListBackwards = iterateAcrossLinkedListBackwards;
+// var linkedList = new LinkedList();
+// linkedList.addToTail('1');
+// linkedList.addToTail(null);
+// linkedList.addToTail('A');
+// linkedList.addToTail(false);
+// linkedList.addToTail(undefined);
+// var result = iterateAcrossLinkedListBackwards(linkedList);
