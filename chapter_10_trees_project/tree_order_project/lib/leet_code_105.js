fd87a28ea197d4a705437d0cd757e11420b3193e
Sean Kennealy <spkennealy@gmail.com>
2020-06-16 09:08:03 -0700

    Leetcode problem 105: buildTree(preorder, inorder)

--- chapter_10_trees_project/tree_order_project/lib/leet_code_105.js
+++ chapter_10_trees_project/tree_order_project/lib/leet_code_105.js
@@ -3,7 +3,46 @@
 
 const { TreeNode } = require('./tree_node.js');
 
+// Given preorder and inorder traversal of a tree, construct the binary tree.
+
+//     Note:
+// You may assume that duplicates do not exist in the tree.
+
+// For example, given
+
+// preorder = [3, 9, 20, 15, 7]
+// inorder = [9, 3, 15, 20, 7]
+// Return the following binary tree:
+
+//      3
+//     / \
+//    9  20
+//      /  \
+//     15   7
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
 
 function buildTree(preorder, inorder) {
+    if (!preorder.length && !inorder.length) return null;
 
+    let root = new TreeNode(preorder[0]);
+    let rootIdx = inorder.indexOf(preorder[0]);
+
+    let leftInorder = inorder.slice(0, rootIdx);
+    let rightInorder = inorder.slice(rootIdx + 1);
+
+    let leftPreorder = preorder.filter(val => leftInorder.includes(val));
+    let rightPreorder = preorder.filter(val => rightInorder.includes(val));
+
+    root.left = buildTree(leftPreorder, leftInorder);
+    root.right = buildTree(rightPreorder, rightInorder);
+
+    return root;
 }
