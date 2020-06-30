0759977e8583727c2cbc065aee5a47be3e8cb17f
Sean Kennealy <spkennealy@gmail.com>
2020-06-30 07:53:58 -0700

    Completed treeSum()

--- chapter_10_trees_project/depth_breadth_first_project/lib/tree_sum.js
+++ chapter_10_trees_project/depth_breadth_first_project/lib/tree_sum.js
@@ -3,7 +3,14 @@ function treeSum(root) {
     if (!root) return count;
 
     let queue = [ root ];
-    
+    while (queue.length) {
+        let node = queue.shift();
+        count += node.val;
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+
+    return count;
 }
 
 
